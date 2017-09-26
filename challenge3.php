a5db91d87bac9a823db96d427247531a2335810e
Miguel Rosas <miguelrosas@Miguels-Mac.local>
2017-09-26 18:29:20 -0400

    omg work damit

--- challenge3.php
+++ challenge3.php
@@ -16,8 +16,9 @@
 </form>
 <?php
 if(!empty($_POST)){
-    $db = new PDO("mysql:dbname=MRosas_SQLchallenge_one;host=localhost","r2hstudent", "SbFaGzNgGIE8kfP");
     try {
+      $db = new PDO("mysql:dbname=MRosas_SQLchallenge_one;host=localhost","r2hstudent", "SbFaGzNgGIE8kfP");
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //query where are you inserting into...
       $query = "INSERT INTO challenge_two (name, description, price, color) VALUES (:name, :description, :price, :color)";
       // prepare the statment
@@ -36,5 +37,3 @@ if(!empty($_POST)){
     }
     }
 ?>
-</body>
-</html>
